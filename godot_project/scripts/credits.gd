e529ab85275b8fbf4992c23aafb46b6474f643e1
Jaime Crespo <jynus@users.noreply.github.com>
2024-08-20 20:17:54 +0200

    Last minute tweaks

--- godot_project/scripts/credits.gd
+++ godot_project/scripts/credits.gd
@@ -3,6 +3,7 @@ extends Node2D
 const TWITTER_URL : String = "https://twitter.com/jynus"
 const ITCHIO_URL : String = "https://jynus.itch.io"
 const WEB_URL : String = "https://jynus.com"
+const GITHUB_URL : String = "https://github.com/jynus/gmtk24"
 
 @onready var done_button : Button = %doneButton
 
@@ -14,7 +15,8 @@ func _ready():
 						"[center][b]" + tr("Music & Sounds") + "[/b]:\n\n" + \
 						"Epidemic Sound\n\n" + \
 						tr("See README for the full attribution list.\n\n") + \
-						tr("* Made with Godot 4 in 4 days for the Game Jam *") + \
+						tr("* Made with Godot 4.3 in 4 days for the GMTK Game Jam 2024 *\n\n") + \
+						tr("Full source code is available at GitHub under the AGPL-3.0 License") + \
 						"[/center]"
 
 func _process(_delta):
@@ -46,3 +48,7 @@ func _on_itchio_button_pressed():
 func _on_web_button_pressed():
 	Fx.click.play()
 	open_web_browser(WEB_URL)
+
+func _on_github_button_pressed() -> void:
+	Fx.click.play()
+	open_web_browser(GITHUB_URL)
